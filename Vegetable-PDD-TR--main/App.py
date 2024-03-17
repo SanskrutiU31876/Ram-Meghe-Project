be792c5bf5039c4ab28698b0878fa94494731c11
SanskrutiU31876 <156110161+SanskrutiU31876@users.noreply.github.com>
2024-03-17 14:36:36 +0530

    Update App.py

--- Vegetable-PDD-TR--main/App.py
+++ Vegetable-PDD-TR--main/App.py
@@ -9,7 +9,7 @@ import cv2
 
 # Load models and class names
 #MODEL = tf.keras.models.load_model('./potato_trained_models/1/')
-#MODEL = tf.keras.models.load_model('tomato.h5')
+MODEL = tf.keras.models.load_model('tomato.h5')
 TOMATO_MODEL = tf.keras.models.load_model('./tomato_trained_models/1')
 #TOMATO_MODEL = tf.keras.models.load_model('tomato.h5')
 PEPPER_MODEL = tf.keras.models.load_model('./pepper_trained_models/1')
